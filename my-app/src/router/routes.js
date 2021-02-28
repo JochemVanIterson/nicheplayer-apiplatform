4f11e7272cf4b0b8c2f6405b0c1a39986c8da2b5
Jochem van Iterson <jochem@audioware.nl>
2021-03-01 00:02:44 +0100

    Better router

--- my-app/src/router/routes.js
+++ my-app/src/router/routes.js
@@ -5,10 +5,18 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      ...greetingRoutes,
     ]
   },
-  ...greetingRoutes,
+  // {
+  //   path: '/admin2',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     ...greetingRoutes
+  //   ]
+  // },
+  
 
   // Always leave this as last one,
   // but you can also remove it
