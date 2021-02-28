cec955ce684238cf0d369c821f3764f4d910c2fd
Jochem van Iterson <jochem@audioware.nl>
2021-03-01 00:03:30 +0100

    typo

--- my-app/src/router/routes.js
+++ my-app/src/router/routes.js
@@ -10,7 +10,7 @@ const routes = [
     ]
   },
   // {
-  //   path: '/admin2',
+  //   path: '/admin',
   //   component: () => import('layouts/MainLayout.vue'),
   //   children: [
   //     ...greetingRoutes
