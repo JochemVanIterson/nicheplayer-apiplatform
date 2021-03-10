0b1f18a97f2f3a433fd636749e121a0a3ae51abb
Jochem <jochem@audioware.nl>
2021-03-10 13:29:51 +0100

    Missing files in last commit

--- api/src/Entity/Album.php
+++ api/src/Entity/Album.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\AlbumRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -51,6 +53,16 @@ class Album
      * @ORM\ManyToOne(targetEntity=Media::class)
      */
     private $albumArt;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Song::class, mappedBy="album")
+     */
+    private $songs;
+
+    public function __construct()
+    {
+        $this->songs = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -101,6 +113,36 @@ class Album
     public function setAlbumArt(?Media $albumArt): self
     {
         $this->albumArt = $albumArt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Song[]
+     */
+    public function getSongs(): Collection
+    {
+        return $this->songs;
+    }
+
+    public function addSong(Song $song): self
+    {
+        if (!$this->songs->contains($song)) {
+            $this->songs[] = $song;
+            $song->setAlbum($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSong(Song $song): self
+    {
+        if ($this->songs->removeElement($song)) {
+            // set the owning side to null (unless already changed)
+            if ($song->getAlbum() === $this) {
+                $song->setAlbum(null);
+            }
+        }
 
         return $this;
     }
