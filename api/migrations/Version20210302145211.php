46353f7e62f4f877589599503ea7a9b1b885d03a
Jochem <jochem@audioware.nl>
2021-03-10 13:24:34 +0100

    add relation album-song

--- api/migrations/Version20210302145211.php
+++ api/migrations/Version20210302145211.php
@@ -19,7 +19,7 @@ final class Version20210302145211 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {        
-        $config = '{ \"path\": \"%kernel.project_dir%/public/media\" }';
+        $config = '{ \"path\": \"%kernel.project_dir%/public/media/\" }';
 
         $this->addSql('INSERT INTO source (id, name, type, config) VALUES (1, "local", "LOCAL", "'.$config.'")');
         // this up() migration is auto-generated, please modify it to your needs
@@ -31,5 +31,9 @@ final class Version20210302145211 extends AbstractMigration
         $this->addSql('DELETE FROM source WHERE id = 1');
         // this down() migration is auto-generated, please modify it to your needs
 
+    }
+    public function isTransactional(): bool
+    {
+        return false;
     }
 }
