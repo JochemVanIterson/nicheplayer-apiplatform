f6b628c8389b35b10c4883d2212049e4433d20cc
Jochem <jochem@audioware.nl>
2021-03-14 18:29:08 +0100

    Show profile picture in admin

--- pwa/pages/admin/Users/index.tsx
+++ pwa/pages/admin/Users/index.tsx
@@ -1,6 +1,7 @@
 import UsersCreate from "./UsersCreate";
 import UsersList from "./UsersList";
 import UsersEdit from "./UsersEdit";
+import UsersShow from "./UsersShow"
 
 import UserIcon from '@material-ui/icons/People';
       
@@ -8,5 +9,6 @@ export default {
     list: UsersList,
     edit: UsersEdit,
     create: UsersCreate,
+    show: UsersShow,
     icon: UserIcon
 };
