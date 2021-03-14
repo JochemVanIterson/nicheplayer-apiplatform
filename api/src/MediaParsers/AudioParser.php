70710c1ccf08e95b8a4c7a552f412ed52d55c7b1
Jochem <jochem@audioware.nl>
2021-03-14 18:29:30 +0100

    Parse metadata

---
+++ api/src/MediaParsers/AudioParser.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\MediaParsers;
+
+use App\MediaParsers\BaseParser;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+require __DIR__.'/../../vendor/james-heinrich/getid3/getid3/getid3.php';
+
+class AudioParser extends BaseParser
+{
+    private $getID3 = null;
+    function __construct(UploadedFile $file) {
+        parent::__construct($file);
+        $this->getID3 = new \getID3;
+    }
+
+    public static function isType(UploadedFile $file): bool
+    {
+        $mime = $file->getMimeType();
+        return str_contains($mime, "audio");
+    }
+
+    public function generateMetadata(): ?array
+    {
+        $meta = parent::generateMetadata();
+
+        $id3Data = $this->getID3->analyze($this->file->getPathname());
+        $this->getID3->CopyTagsToComments($id3Data);
+
+        $meta['type'] = "audio";
+        $meta['artist'] = $id3Data['comments_html']['artist'][0];
+        $meta['title'] = $id3Data['tags']['id3v2']['title'][0];
+
+        return $meta;
+    }
+}
