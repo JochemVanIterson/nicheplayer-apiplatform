5d70df827b0829d14e9ee6ccd1dac752dac3f72b
Jochem <jochem@audioware.nl>
2021-03-14 15:58:42 +0100

    Custom mediaobject views

--- pwa/pages/admin/MediaObjects/index.tsx
+++ pwa/pages/admin/MediaObjects/index.tsx
@@ -1,8 +1,14 @@
 import MediaObjectsCreate from "./MediaObjectsCreate";
+import MediaObjectsList from "./MediaObjectsList";
+import MediaObjectsEdit from "./MediaObjectsEdit";
+import MediaObjectsShow from "./MediaObjectsShow";
 
 import PhotoIcon from '@material-ui/icons/Photo';
       
 export default {
     create: MediaObjectsCreate,
+    list: MediaObjectsList,
+    edit: MediaObjectsEdit,
+    show: MediaObjectsShow,
     icon: PhotoIcon
 };
