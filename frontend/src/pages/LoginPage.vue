c200c924a97a1f7f39a78d2b0bfbbbbdf73a48b6
Jochem <jochem@audioware.nl>
2021-04-03 23:49:20 +0200

    Loginpage layout

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,17 +1,18 @@
 <template lang="pug">
   q-layout(view="lHh Lpr lFf")
     q-page-container
-      q-page.flex.flex-center.column.bg-grey-4(padding)
-        img( alt="Quasar logo" src="~assets/app-logo.svg" height="128px")
-        q-card.q-mt-lg#loginCard
-          q-card-section
-            .text-h6 Login
-          q-card-section
-            q-input(v-model="username" label="Username" @keydown.enter.prevent="attemptLogin")
-            q-input(v-model="password" label="Password" type="password" @keydown.enter.prevent="attemptLogin")
-          q-card-actions(align="right")
-            q-btn(flat color="primary") Go back
-            q-btn(color="primary" @click="attemptLogin") Login
+      q-page.flex.flex-center.column.bg-grey-4()
+        q-img.bg-grey-4.window-height(src="http://mbp.audioware.nl/api/media/6061bc0b83806_IMG_20180720_074013.jpg")
+          div(class="absolute-full text-subtitle2 flex flex-center bg-transparent")
+            q-card.q-mt-lg#loginCard
+              q-card-section
+                .text-h4 Login
+              q-card-section.q-gutter-y-md
+                q-input(bg-color="white" filled v-model="username" label="Username" @keydown.enter.prevent="attemptLogin")
+                q-input(bg-color="white" filled v-model="password" label="Password" type="password" @keydown.enter.prevent="attemptLogin")
+              q-card-actions(align="right")
+                q-btn(color="primary" @click="$router.go(-1)") Go back
+                q-btn(color="primary" @click="attemptLogin") Login
 </template>
 
 <script>
@@ -56,5 +57,7 @@ export default {
   #loginCard {
     width: 100%;
     max-width: 400px;
+    color: white;
+    background-color: #222222A0;
   }
 </style>
--- frontend/src/pages/RegisterPage.vue
+++ frontend/src/pages/RegisterPage.vue
@@ -1,20 +1,22 @@
 <template lang="pug">
   q-layout(view="lHh Lpr lFf")
     q-page-container
-      q-page.flex.flex-center.column.bg-grey-4(padding)
-        q-card.q-mt-lg#registerCard
-          q-card-section
-            .text-h4 Register
-          q-card-section.q-gutter-y-md
-            .row.q-col-gutter-x-sm
-              q-input.col(outlined v-model="firstname" label="First name")
-              q-input.col(outlined v-model="lastname" label="Last name")
-            q-input(outlined v-model="username" label="Username")
-            q-input(outlined v-model="email" label="Email" type="email")
-            q-input(outlined v-model="password" label="Password" type="password")
-          q-card-actions(align="right")
-            q-btn(flat color="primary") Go back
-            q-btn(color="primary" @click="registerAction") Register
+      q-page.flex.flex-center.column.bg-grey-4
+        q-img.bg-grey-4.window-height(src="http://mbp.audioware.nl/api/media/6061bc0b83806_IMG_20180720_074013.jpg")
+          div(class="absolute-full text-subtitle2 flex flex-center bg-transparent")
+            q-card.q-mt-lg.bg-white#registerCard
+              q-card-section
+                .text-h4.text-black Register
+              q-card-section.q-gutter-y-md
+                .row.q-col-gutter-x-sm
+                  q-input.col(outlined v-model="firstname" label="First name")
+                  q-input.col(outlined v-model="lastname" label="Last name")
+                q-input(outlined v-model="username" label="Username")
+                q-input(outlined v-model="email" label="Email" type="email")
+                q-input(outlined v-model="password" label="Password" type="password")
+              q-card-actions(align="right")
+                q-btn(flat color="primary" @click="$router.go(-1)") Go back
+                q-btn(color="primary" @click="registerAction") Register
 </template>
 
 <script>
